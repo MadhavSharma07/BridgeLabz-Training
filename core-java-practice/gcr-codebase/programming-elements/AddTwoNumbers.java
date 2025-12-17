7dc0c1e0987111a897cba87b8552ffdb88ccff31
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-17 16:08:09 +0530

    [Madhav Sharma]Add. add AddTwoNumbers program

---
+++ core-java-practice/gcr-codebase/programming-elements/AddTwoNumbers.java
@@ -0,0 +1,17 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements;
+import java.util.*;
+
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number:");         // input first number from user
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number:");      // input second number from user
+        int num2 = sc.nextInt();
+        int sum=0;                  // store sum of given numbers
+
+        sum = num1+num2; // addition
+        System.out.print("The sum of"+a+"and"+b+"is:"+sum);         // print sum
+    }
+}
