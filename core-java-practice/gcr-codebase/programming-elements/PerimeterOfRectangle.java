ebba429da764c76e1f95d03baf3a14d915f845a9
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-17 16:47:12 +0530

    [Madhav Sharma]Add. add perimeter of rectangle program

---
+++ core-java-practice/gcr-codebase/programming-elements/PerimeterOfRectangle.java
@@ -0,0 +1,15 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements;
+ 
+import java.util.*;
+
+public class PerimeterOfRectangle {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the length of rectangle:");
+    double length= sc.nextDouble();             // take input length from user
+    System.out.print("Enter the breadth of rectangle:");
+    double breadth= sc.nextDouble();             // take input breadth from user
+    double perimeter=0;         // to store the perimeter of rectangle
+     
+    perimeter  =2*(length+breadth);       // formula
+    System.out.print("The perimeter of rectangle is:"+perimeter);     // print perimeter of rectangle
+}
