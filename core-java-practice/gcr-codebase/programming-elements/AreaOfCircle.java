535c07a86bed7f87cf893fd26ee692018245da2a
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-17 16:53:19 +0530

    [Madhav Sharma]Refactor. modify area of circle program

--- core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -3,13 +3,17 @@ package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements
 import java.util.*;
 
 public class AreaOfCircle {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the radius:");
-    double radius= sc.nextDouble();             // take input radius from user
-    double area=0;         // to store the area  of circle
+    public static void main(String[] args) {
+        
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius:");
+        double radius= sc.nextDouble();             // take input radius from user
+        double area=0;         // to store the area  of circle
      
-    area  =(3.14*radius*radius);       // formula
-    System.out.print("The area of circle is:"+area);     // print area of circle
-
+        area  =(3.14*radius*radius);       // formula
+        System.out.print("The area of circle is:"+area);     // print area of circle
+    
+    }
 
 }
