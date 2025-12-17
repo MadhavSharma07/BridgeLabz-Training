5b296af8b34445cd5d2dd59b55e8b809ed91fb24
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-17 15:58:25 +0530

    [Madhav Sharma]Add. commit Welcome Program

---
+++ core-java-practice/gcr-codebase/programming-elements/Welcome.java
@@ -0,0 +1,7 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Bridgelabz!");
+    }
+}
