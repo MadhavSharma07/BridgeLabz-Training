3a0f11b567832ca300825d53664170fe410a4b82
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-17 22:30:38 +0530

    [Madhav Sharma]Refactor. modify simple interest program

--- core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
+++ core-java-practice/gcr-codebase/programming-elements/SimpleInterest.java
@@ -3,16 +3,20 @@ package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements
 
 import java.util.*;
 public class SimpleInterest {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the principal amount:");
-    double principal = sc.nextDouble(); // take input principal from user
-    System.out.print("Enter the rate:");
-    double rate = sc.nextDouble(); // take input rate from user
-    System.out.print("Enter the time:");
-    double time = sc.nextDouble(); // take input time from user
-    double simpleInterest=0;         // store the simple interest
+    public static void main(String[] args) {
+        
+    
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the principal amount:");
+        double principal = sc.nextDouble(); // take input principal from user
+        System.out.print("Enter the rate:");
+        double rate = sc.nextDouble(); // take input rate from user
+        System.out.print("Enter the time:");
+        double time = sc.nextDouble(); // take input time from user
+        double simpleInterest=0;         // store the simple interest
 
-    simpleInterest  =(principal * rate* time )/100;       // formula
-    System.out.print("The simple interest is:"+simpleInterest);     // print simple interest
+        simpleInterest  =(principal * rate* time )/100;       // formula
+        System.out.print("The simple interest is:"+simpleInterest);     // print simple interest
+    }
 
 }
