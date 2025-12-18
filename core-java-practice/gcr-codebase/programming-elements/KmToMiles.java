f0629d7240f6a3bb05e931b82012bcca52d9e60e
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-18 11:10:51 +0530

    [Madhav Sharma]Add. add conversion of kilometer to miles program

---
+++ core-java-practice/gcr-codebase/programming-elements/KmToMiles.java
@@ -0,0 +1,11 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        double kilometer_distance = 10.8; // distance in kilometer
+        double miles_distance = kilometer_distance*1.6;   // conversion kilometer to miles
+        
+        System.out.print("Distance of"+kilometer_distance+" km in miles is "+miles_distance); // print distance in miles
+
+    }
+}
