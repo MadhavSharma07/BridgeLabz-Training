3d4d1c1693f7a65dbc07d16ecce74a4b40aaf2f3
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-18 10:52:26 +0530

    [Madhav Sharma]Add. add Harry's age  program

---
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -0,0 +1,13 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.programming-elements;
+import java.util.*;
+
+public class AgeOfHarry {
+    public static void main(String[] args) {
+        int birthyear =2000;  // birthyear
+        int currentyear= 2024;   // currentyear
+        int age = currentyear - birthyear; // calculate age of Harry
+
+        System.out.print("Harry's age in 2024 is "+age);
+
+    }
+}
