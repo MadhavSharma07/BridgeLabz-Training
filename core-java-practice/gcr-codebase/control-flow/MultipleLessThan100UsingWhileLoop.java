6f8c4a449bdb0e1f56ae5917f57f8decde7cdf20
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-20 00:35:15 +0530

    [Madhav Sharma]Add. find  multiples less than 100 of given number using while loop   program

---
+++ core-java-practice/gcr-codebase/control-flow/MultipleLessThan100UsingWhileLoop.java
@@ -0,0 +1,25 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.control-flow;
+
+import java.util.*;
+public class MultipleLessThan100UsingWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the number(between 1-100):");
+
+        int number = sc.nextInt();       // take number as input from user
+
+        int counter = number-1;
+
+        if(number>0 && number<=100){
+            while(counter>1){      // loop iterates from 100 to 1
+                if(number%counter==0){              // number is divided by counter
+                    System.out.println("The multiples of "+number+" are "+counter);     // print the output
+                }
+            }
+        }
+
+        else{
+            System.out.println("Enter the valid input");
+        }
+    }
+}
