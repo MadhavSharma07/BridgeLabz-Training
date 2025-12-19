ea1c362639ac03e5e60740bd64f4322fdc81090f
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-20 00:20:03 +0530

    [Madhav Sharma]Add. find  multiples less than 100 of given number using for loop   program

---
+++ core-java-practice/gcr-codebase/control-flow/MultipleLessThan100.java
@@ -0,0 +1,20 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.control-flow;
+
+
+import java.util.*;
+public class MultipleLessThan100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the number:");
+
+        int number = sc.nextInt();       // take number as input from user
+
+        for(int i =100;i>=1;i--){      // loop iterates from 100 to 1
+            if(number%i==0){              // number is divided by i
+                System.out.println("The multiples of "+number+" are "+i);     // print the output
+            }
+        }
+
+
+    }
+}
