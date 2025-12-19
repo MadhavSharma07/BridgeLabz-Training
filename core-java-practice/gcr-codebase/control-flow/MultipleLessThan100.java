0282c4074268d7c531ee4a06124e41a45a0d1687
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-20 00:30:06 +0530

    [Madhav Sharma]Refactor. modify control flow using else   program

--- core-java-practice/gcr-codebase/control-flow/MultipleLessThan100.java
+++ core-java-practice/gcr-codebase/control-flow/MultipleLessThan100.java
@@ -5,16 +5,23 @@ import java.util.*;
 public class MultipleLessThan100 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
-        System.out.print("Enter the number:");
+        System.out.print("Enter the number(between 1-100):");
 
         int number = sc.nextInt();       // take number as input from user
 
-        for(int i =100;i>=1;i--){      // loop iterates from 100 to 1
-            if(number%i==0){              // number is divided by i
-                System.out.println("The multiples of "+number+" are "+i);     // print the output
+        if(number>0 && number<=100){
+            for(int i =100;i>=1;i--){      // loop iterates from 100 to 1
+                if(number%i==0){              // number is divided by i
+                    System.out.println("The multiples of "+number+" are "+i);     // print the output
+                }
             }
         }
 
-
+        else{
+            System.out.println("Enter the valid input");
+        }
     }
+
+
+    
 }
