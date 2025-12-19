03a95afd9e2531431349f60838e0b6c024a3c1a5
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-19 10:51:55 +0530

    [Madhav Sharma]Refactor. modify age of harry program

--- core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class AgeOfHarry {
     public static void main(String[] args) {
         int birthYear =2000;  // birthyear
-        int currentyear= 2024;   // currentyear
-        int age = currentyear - birthYear; // calculate age of Harry
+        int currentYear= 2024;   // currentyear
+        int age = currentYear - birthYear; // calculate age of Harry
 
         System.out.print("Harry's age in 2024 is "+age);
 
