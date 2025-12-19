2fe94005fc702fa03bb48011d939af6fd6e8fcfe
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-19 16:57:21 +0530

    [Madhav Sharma]Add. sum of number until user gives input zero or neagtive number program

---
+++ core-java-practice/gcr-codebase/control-flow/SumOfNumberUntilNegative.java
@@ -0,0 +1,24 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.control-flow;
+
+import java.util.*;
+
+public class SumOfNumberUntilNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        boolean flag =true;
+
+        int sum=0;
+
+        while(flag){
+            System.out.print("Enter the number:");
+            int number = sc.nextInt(); // take number as input from user
+            if(number<=0){
+                break;
+            }
+            sum+=number;
+        }
+
+        System.out.println(sum);
+    }
+}
