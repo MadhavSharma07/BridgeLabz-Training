10ddb160780ace8a8a80a7b18f22886a04d1c410
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-20 11:55:42 +0530

    [Madhav Sharma]Add. check Abundant Number  program

---
+++ core-java-practice/gcr-codebase/control-flow/AbundantNumber.java
@@ -0,0 +1,25 @@
+package BridgeLabz-Training.core-java-practice.gcr-codebase.control-flow;
+
+import java.util.*:
+
+public class AbundantNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");     // take input
+
+        int number = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum > number) {
+            System.out.println("Number is an Abundant Number");     // prin output
+        } else {
+            System.out.println(" Number is not an Abundant Number");
+        }
+    }
+}
