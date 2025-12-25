a231c3089df0500ed7ba7382ed01990ea8f78735
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2025-12-25 22:53:14 +0530

    [Madhav Sharma]Add. Bank Acoount  program

--- oops-practice/gcr-codebase/java-keywords/BankAccount.java
+++ oops-practice/gcr-codebase/java-keywords/BankAccount.java
@@ -7,11 +7,13 @@ public class BankAccount {
     
     private final int accountNumber;      // final account number variable 
     private String accountHolderName;       // final holder name variable
+    private double balance;
 
     
-    public BankAccount(int accountNumber, String accountHolderName) {     // Constructor
+    public BankAccount(int accountNumber, String accountHolderName,double balance) {     // Constructor
         this.accountNumber = accountNumber;
         this.accountHolderName = accountHolderName;
+        this.balance= balance;
         totalAccounts++;
     }
 
@@ -26,12 +28,16 @@ public class BankAccount {
         System.out.println("Account Number: " + accountNumber);
         System.out.println("Account Holder: " + accountHolderName);
     }
+    public double getBalance() {      // method to print balance
+       return balance;
+   }
+
 
     
     public static void main(String[] args) {
 
-        BankAccount account1 = new BankAccount(01, "Aditya"); // create object aacount 1
-        BankAccount account2 = new BankAccount(02, "Sarthak");// create object account 2
+        BankAccount account1 = new BankAccount(01, "Aditya",100); // create object aacount 1
+        BankAccount account2 = new BankAccount(02, "Sarthak",300);// create object account 2
 
         
         if (account1 instanceof BankAccount) {          // instanceof check before displaying details
@@ -44,6 +50,10 @@ public class BankAccount {
             account2.displayDetails();
         }
 
+        System.out.println("\nThe balance in  account1 :");
+        account1.getBalance();
+        System.out.println("\nThe balance in  account2 :");
+        account2.getBalance();
         
         BankAccount.getTotalAccounts();
     }
