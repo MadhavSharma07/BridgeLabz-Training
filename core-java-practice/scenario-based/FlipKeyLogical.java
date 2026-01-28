ce30a189c2c4aa2cf301b5ce439337e2154d7331
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2026-01-28 12:27:26 +0530

    [Madhav Sharma]Refactor . add comments to Flip Key Logical scenario program

--- core-java-practice/scenario-based/FlipKeyLogical.java
+++ core-java-practice/scenario-based/FlipKeyLogical.java
@@ -7,26 +7,26 @@ public class FlipKeyLogical {
     	Scanner sc = new Scanner(System.in);
     	System.out.println("Enter the input : ");
     	String str = sc.nextLine();
-    	if(cleanseAndInvert(str)=="") {
+    	if(cleanseAndInvert(str)=="") {  // output is empty string 
     		System.out.println("Invalid Input");
     	}else {
-    	System.out.println("The generated key is - "+cleanseAndInvert(str));
+    	System.out.println("The generated key is - "+cleanseAndInvert(str)); // print specified output
     	}
     }
     
     public  static String cleanseAndInvert(String input) {
-    	if(input.length()<6 || input==null) {
+    	if(input.length()<6 || input==null) {  // condition for input has less than 6 characters  or null
     		return "";
     	}
-    	if(input.trim().contains(" ") || input.trim().matches(".*(\\d|[^a-zA-Z]).*")) {
+    	if(input.trim().contains(" ") || input.trim().matches(".*(\\d|[^a-zA-Z]).*")) {  // condition for input has space, digits or special characters
     		return "";
     	}
     	
-    	input = input.toLowerCase();
+    	input = input.toLowerCase();  // change string to lowercase
     	
     	String ans="";
     	for(int i=0;i<input.length();i++) {
-    		if((int)input.charAt(i)%2==0) {
+    		if((int)input.charAt(i)%2==0) {  // remove even ASCII values character
     			continue;
     		}
     		else {
@@ -36,17 +36,17 @@ public class FlipKeyLogical {
     	
     	String reverseString="";
     	for(int i=ans.length()-1;i>=0;i--) {
-    		reverseString+=ans.charAt(i);
+    		reverseString+=ans.charAt(i);  // reverse the string
     	}
     	
     	String resultantString ="";
     	for(int i=0;i<reverseString.length();i++) {
-    		if(i%2==0) {
-    		resultantString+=(char)(reverseString.charAt(i)-32)+"";
+    		if(i%2==0) {  // condition for  even index
+    		resultantString+=(char)(reverseString.charAt(i)-32)+""; // change to uppercase and add in string
     		
     		}
-    		else {
-    			resultantString+=reverseString.charAt(i)+"";
+    		else { // odd index
+    			resultantString+=reverseString.charAt(i)+"";  // add in string in lowercase 
     		}
     	}
     	
