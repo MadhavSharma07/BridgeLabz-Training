ada34f6de7fda074aca5de3d2715f059e5046ed6
MadhavSharma07 <122592334+MadhavSharma07@users.noreply.github.com>
2026-02-16 15:32:59 +0530

    [Madhav Sharma]Add . Address Book Problem program

---
+++ oops-practice/gcr-codebase/address-book/dao/AddressBookDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import model.Contact;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookDAO {
+
+    private List<Contact> contactList = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contactList.add(contact);
+    }
+
+    public List<Contact> getAllContacts() {
+        return contactList;
+    }
+
+    public void deleteContact(String name) {
+        for (int i = 0; i < contactList.size(); i++) {
+            if (contactList.get(i).getFirstName().equalsIgnoreCase(name)) {
+                contactList.remove(i);
+                break;
+            }
+        }
+    }
+}
